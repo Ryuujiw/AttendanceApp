31e2687f2f24d304b2ac3935a3fcf02265b50954
dingsheng <dingsheng0166@gmail.com>
2018-11-16 00:49:32 +0800

    link course to course details

--- app/src/main/java/com/example/ryuu/attendanceapp/CourseDetails.java
+++ app/src/main/java/com/example/ryuu/attendanceapp/CourseDetails.java
@@ -1,13 +1,28 @@
 package com.example.ryuu.attendanceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CourseDetails extends AppCompatActivity {
+
+    Button btn_done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+        btn_done = findViewById(R.id.btn_create);
+
+        btn_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CourseDetails.this , ClassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ryuu/attendanceapp/CreateCourseActivity.java
+++ app/src/main/java/com/example/ryuu/attendanceapp/CreateCourseActivity.java
@@ -1,13 +1,28 @@
 package com.example.ryuu.attendanceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateCourseActivity extends AppCompatActivity {
+
+    Button btn_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
+
+        btn_create = findViewById(R.id.btn_create);
+
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateCourseActivity.this, CourseDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
