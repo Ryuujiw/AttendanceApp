d2d93c28ea680b1f98bc6992b9e481c0987fbdae
durasis96 <lehv19960831@gmail.com>
2018-11-18 16:39:09 +0800

    edit profile , my profile

---
+++ app/src/main/java/com/example/ryuu/attendanceapp/activity_edit_profile.java
@@ -0,0 +1,13 @@
+package com.example.ryuu.attendanceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_edit_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
