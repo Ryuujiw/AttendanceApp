aa499b752df585a890ac107832103c45181e87b3
mengfong96 <mingfengnyf@gmail.com>
2018-12-20 04:47:57 +0800

    working on mail intent

--- app/src/main/java/com/example/ryuu/attendanceapp/AttendanceFragment.java
+++ app/src/main/java/com/example/ryuu/attendanceapp/AttendanceFragment.java
@@ -28,13 +28,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.ryuu.attendanceapp.objects.Class;
-import com.example.ryuu.attendanceapp.objects.Classes;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.model.Message;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
